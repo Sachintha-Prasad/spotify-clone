d3b2990b3d785a03d5e468ad4516f6746d6fcae7
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-05-12 22:10:19 +0530

    main display

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Sidebar from "./components/Sidebar"
 import Player from "./components/Player"
+import Display from "./components/Display"
 
 const App = () => {
     return (
         <div className="h-screen bg-black font-montserrat text-base">
             <div className="h-[90%] flex">
                 <Sidebar />
+                <Display />
             </div>
             <Player />
         </div>
